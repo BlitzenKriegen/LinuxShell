af8c33ce42b24365f74383378127e9e139415825
BlitzenKriegen <kirils@hotmail.ca>
2022-10-12 00:47:48 +0000

    Stub of Basic functionality

--- makefile
+++ makefile
@@ -1,8 +1,16 @@
 OBJ = dir.o
 
-shell: $(OBJ)
-	gcc -g $(OBJ) -o sh
+sh: dir.o shell.o file.o
+	gcc -g dir.o -o dir
+	gcc -g shell.o -o sh
+	gcc -g file.o -o fl
 	rm *.o *~
+
+shell.o: shell.c shell.h
+	gcc -g -c shell.c
 
 dir.o: dir.c dir.h
 	gcc -g -c dir.c
+
+file.o: file.c file.h
+	gcc -g -c file.c
---
+++ shell.h
@@ -0,0 +1,16 @@
+/*
+  Names: Kiril S.
+         Enrik R
+
+  Module: Shell
+
+  Purpose: The shell module is meant to hold the nominal
+  shell program that faces outward towards the user. When the
+  user inputs a command, the shell module will process the input
+  and then delegate the given command to the relavent module "family"
+  (i.e the file module for a file command, directory module for a dir
+  command, etc).
+
+  Details: The module awaits user input and the processes their input.
+  (FINISH LATER).
+ */
