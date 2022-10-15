448b9ab9cf923976b324838ee188e9550a26c51d
BlitzenKriegen <kirils@hotmail.ca>
2022-10-15 03:45:07 +0000

    basic directory stub

---
+++ dircmd/dir.h
@@ -0,0 +1,18 @@
+/*
+  Names: Kiril S.
+         Enrik R
+
+  Module: Directory Commands
+
+  Purpose: The directory module is meant to
+  catalog and hold to functions to perform directory-
+  level commands within the shell implementations, such
+  as creating/destroying a directory and changing directory.
+
+  Details: The module is activated by a call to one of its
+  functions in the shell prompt, which then passes in the
+  arguements made by the user to to program, which would then
+  process them.
+ */
+
+#include <stdio.h>
