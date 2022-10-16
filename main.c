ec332e0509481b1ddaf15b449f0bf5ae46034d85
Enrik Rushiti <erush902@mtroyal.ca>
2022-10-16 02:52:20 +0000

    login works but loop does not

--- main.c
+++ main.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
 #define USERLENGTH 31
-
+#define CLLENGTH 256
 
 int main(){
 
   char userName[USERLENGTH];
   char userPass[USERLENGTH];
   
-  printf("Welcome to the Linux Shell!\n");
-  printf("Enter your username (30 characters max): ");
-  scanf("%s", userName);
-  printf("\nEnter your password (30 characters max): ");
-  scanf("%s", userPass);
+  write(1, "Welcome to the Linux Shell!\n", 28);
+  write(1, "Enter your username (30 characters max): ", 41);
+  
+  read(0, userName, USERLENGTH);
+  write(1, "Enter your password (30 characters max): ", 41);
+
+  read(0, userPass, USERLENGTH);
 
   /* will need to have error check eventually */
-  printf("Successfully logged in, Welcome %s!\n", userName);
-  
-  
+  write(1, "Successfully logged in, Welcome!\n", 33);
 
+  char commandLineInput[CLLENGTH];
+  read(0, commandLineInput, CLLENGTH);
+  
+  
+  while (commandLineInput != "exit")
+  {
+    /* processes command line */
+
+
+    read(0, commandLineInput, CLLENGTH);
+  }
+  
   return 0;
 }
